6b22e8bc4081572ab2adce0dd99c080e0d72c99a
Krl5to <i.petretskiy@gmail.com>
2023-08-19 11:54:32 +0300

    More optimises

--- src/test/java/guru/qa/HW05DragAndDropTest.java
+++ src/test/java/guru/qa/HW05DragAndDropTest.java
@@ -1,12 +1,12 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HW05DragAndDropTest {
@@ -14,16 +14,6 @@ public class HW05DragAndDropTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-    }
-
-    @Test
-    void dragAndDropCommand() {
-        open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a header").shouldHave(text("A"));
-        $("#column-b header").shouldHave(text("B"));
-        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
-        $("#column-a header").shouldHave(text("B"));
-        $("#column-b header").shouldHave(text("A"));
     }
 
     @Test
@@ -36,4 +26,14 @@ public class HW05DragAndDropTest {
         $("#column-a header").shouldHave(text("B"));
         $("#column-b header").shouldHave(text("A"));
     }
+    @Test
+    void dragAndDropCommand() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a header").shouldHave(text("A"));
+        $("#column-b header").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+    }
+
 }
